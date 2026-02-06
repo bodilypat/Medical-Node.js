d4d95f6584519938dfed3a82cc3f24dea25b0eb7
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-06 00:09:55 -0800

    Delete backend directory

--- backend/config/env.php
+++
@@ -1,9 +0,0 @@
-<?php
-	return [
-		define('DB_HOST', 'localhost');
-		define('DB_NAME', 'dbmedical');
-		define('DB_USER', 'root');
-		define('DB_PASS', '');
-	];
-	
-	
--- backend/helpers/response.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	function sendJSON($statusCode, $data) {
-		http_response_code($statusCode);
-		header('Content-Type: application/json');
-		echo json_encode($data);
-		exit;
-	}
-	
