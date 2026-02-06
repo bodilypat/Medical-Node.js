1c58a43aa5b0db53d44f35a590b2ff558f2ef193
bodilypat <139784676+bodilypat@users.noreply.github.com>
2026-02-06 00:13:10 -0800

    Create Patient model with schema definition

---
+++ backend/src/models/Patient.js
@@ -0,0 +1,38 @@
+//src/models/Patient.js 
+
+const mongoose = require('mongoose');
+
+const patientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    medicalHistory: {
+        type: String,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('Patient', patientSchema);
+
